ba7242b1777ddf7e455f8fa49c49c8eb586e9a40
Pradyumna Meena <pradyumnameena10375@gmail.com>
2024-02-03 21:41:58 +0530

    Citizenship Act progressed substantially

---
+++ laws-orders/helpers/act.js
@@ -0,0 +1,42 @@
+function swap(id1,id2) {
+	let cont1 = document.getElementById(id1).innerHTML;
+	let cont2 = document.getElementById(id2).innerHTML;
+	document.getElementById(id1).innerHTML = cont2;
+	document.getElementById(id2).innerHTML = cont1;
+}
+
+function visibility(id1,status){
+	var elements = document.getElementsByClassName(id1);
+	var action = "none";
+	if(status=="1"){
+		action = "inline";
+	}
+	for(var i = 0;i<elements.length;i++){
+		elements[i].style.display = action;
+	}
+}
+
+function omissionswap(class_name, id, status){
+	var element1 = document.getElementsByClassName(class_name)[0];
+	var element2 = document.getElementById(id);
+	var action1 = "inline";
+	var action2 = "none";
+	if(status==0){
+		action1 = "none";
+		action2 = "inline";
+	}
+	let cont1 = element1.innerHTML;
+	let cont2 = element2.innerHTML;
+	element1.innerHTML = cont2;
+	element2.innerHTML = cont1;
+	element1.style.display = action1;
+	element2.style.display = action2;
+}
+
+function addcontent(id){
+
+}
+
+function removecontent(id){
+
+}
